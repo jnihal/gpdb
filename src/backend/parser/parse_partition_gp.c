efa34f63a932ea3096f20b89a373f385861d94c9
Nihal Jain <jnihal@vmware.com>
2023-05-22 11:58:43 +0530

    Revert "Remove duplicated if (boundspec->partEvery) (#15585)"
    This reverts commit 21e66e9edb803aa180a8daf8c9d010a3cc7a2d24.

--- src/backend/parser/parse_partition_gp.c
+++ src/backend/parser/parse_partition_gp.c
@@ -1438,6 +1438,8 @@ transformGpPartDefElemWithRangeSpec(ParseState *pstate, Relation parentrel, GpPa
 						parser_errposition(pstate, boundspec->location)));
 		every = linitial(boundspec->partEvery);
 	}
+	else if (boundspec->partEvery)
+		new_boundspec->partEvery = NIL;
 
 	part_col_typid = get_partition_col_typid(partkey, 0);
 	part_col_typmod = get_partition_col_typmod(partkey, 0);
