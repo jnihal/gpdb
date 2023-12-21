56f535f63c0f82746f13c9c6614676a82f3c8949
Nihal Jain <jnihal@vmware.com>
2023-12-21 11:49:45 +0530

    Pipeline changes

--- gpMgmt/bin/go-tools/ci/scripts/run_integration_tests.bash
+++ gpMgmt/bin/go-tools/ci/scripts/run_integration_tests.bash
@@ -2,18 +2,13 @@
 
 set -eux -o pipefail
 
+multihost="${1:-false}"
+
 ccp_src/scripts/setup_ssh_to_cluster.sh
 
 scp cluster_env_files/hostfile_all cdw:/tmp
 tar -xzf gp_binary/gp.tgz
 scp gp cdw:/home/gpadmin/
-
-
-# Install patchelf. We need to SSH as root, hence the use of
-# cluster_env_files.
-ssh -t ccp-$(cat cluster_env_files/terraform/name)-0 "sudo bash -c '
-    source /home/gpadmin/gpdb_src/concourse/scripts/common.bash
-'"
 
 ssh -n cdw "
     set -eux -o pipefail
@@ -26,6 +21,9 @@ ssh -n cdw "
     cd /home/gpadmin/gpdb_src/gpMgmt/bin/go-tools
     ./ci/scripts/generate_ssl_cert_multi_host.bash
 
-    # Run integration tests for multi host with host file
-    make integration-test FILE=/tmp/hostfile_all
+    if "$multihost"; then
+        make integration-test FILE=/tmp/hostfile_all
+    else
+        make integration-test
+    fi
 "
