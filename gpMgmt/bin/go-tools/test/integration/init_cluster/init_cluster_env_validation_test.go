a241fd8d0fcd71786668d4fc10c0e68b7f45ea30
srividya <srividya.ky@gmail.com>
2024-01-25 12:31:51 +0530

    Added new two cluster environment validation cases

--- gpMgmt/bin/go-tools/test/integration/init_cluster/init_cluster_env_validation_test.go
+++ gpMgmt/bin/go-tools/test/integration/init_cluster/init_cluster_env_validation_test.go
@@ -89,4 +89,102 @@ func TestEnvValidation(t *testing.T) {
 			t.Fatalf("got %q, want %q", result.OutputMsg, expectedOut)
 		}
 	})
+	t.Run("when the initdb does not have appropriate permission", func(t *testing.T) {
+		var value cli.Segment
+		var ok bool
+
+		configFile := testutils.GetTempFile(t, "config.json")
+		config := GetDefaultConfig(t)
+
+		err := config.WriteConfigAs(configFile)
+		if err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+
+		coordinator := config.Get("coordinator")
+		if value, ok = coordinator.(cli.Segment); !ok {
+			t.Fatalf("unexpected data type for coordinator %T", value)
+		}
+
+		initdbFilePath := "/usr/local/gpdb/bin/initdb"
+
+		// Open the existing initdb file
+		file, err := os.OpenFile(initdbFilePath, os.O_RDONLY, 0)
+		if err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+		defer func() {
+			// Teardown function to change file permissions back to 755
+			err := file.Chmod(0755)
+			if err != nil {
+				t.Fatalf("unexpected error during changing initdb file permission: %#v", err)
+			}
+			file.Close()
+		}()
+
+		// Set insufficient permissions (for example, read-only)
+		err = file.Chmod(0444)
+		if err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+
+		result, err := testutils.RunInitCluster(configFile)
+		if e, ok := err.(*exec.ExitError); !ok || e.ExitCode() != 1 {
+			t.Fatalf("got %v, want exit status 1", err)
+		}
+
+		expectedOut := fmt.Sprintf("[ERROR]:-host: %s, file %s does not have execute permissions", value.Hostname, initdbFilePath)
+		if !strings.Contains(result.OutputMsg, expectedOut) {
+			t.Fatalf("got %q, want %q", result.OutputMsg, expectedOut)
+		}
+	})
+
+	t.Run("when data directory is not empty and --force is given for gp init command", func(t *testing.T) {
+		var value cli.Segment
+		var ok bool
+
+		configFile := testutils.GetTempFile(t, "config.json")
+		config := GetDefaultConfig(t)
+
+		err := config.WriteConfigAs(configFile)
+		if err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+
+		coordinator := config.Get("coordinator")
+		if value, ok = coordinator.(cli.Segment); !ok {
+			t.Fatalf("unexpected data type for coordinator %T", value)
+		}
+
+		err = os.MkdirAll(value.DataDirectory, 0777)
+		if err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+
+		_, err = os.Create(filepath.Join(value.DataDirectory, "abc.txt"))
+		if err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+
+		commonParentDir := filepath.Dir(value.DataDirectory)
+		commonParentPattern := filepath.Join(commonParentDir, "*")
+
+		defer func() {
+			dirs, err := filepath.Glob(commonParentPattern)
+
+			if err != nil {
+				t.Fatalf("Error listing directories: %#v", err)
+				return
+			}
+			for _, dir := range dirs {
+				os.RemoveAll(dir)
+			}
+		}()
+
+		result, err := testutils.RunInitCluster("--force", configFile)
+		expectedOut := fmt.Sprintf("[INFO]:-Cluster initialized successfully")
+		if !strings.Contains(result.OutputMsg, expectedOut) {
+			t.Fatalf("got %q, want %q", result.OutputMsg, expectedOut)
+		}
+	})
 }
